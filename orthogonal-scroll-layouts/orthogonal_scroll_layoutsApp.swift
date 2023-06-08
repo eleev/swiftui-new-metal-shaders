9de8be067cf4c86c2edb3e486a5dfc27e548b01b
Astemir Eleev <astemireleev@gmail.com>
2023-06-08 14:38:11 -0700

    Initial Commit

---
+++ orthogonal-scroll-layouts/orthogonal_scroll_layoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  orthogonal_scroll_layoutsApp.swift
+//  orthogonal-scroll-layouts
+//
+//  Created by Astemir Eleev on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct orthogonal_scroll_layoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
